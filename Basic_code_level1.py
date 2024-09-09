5a9d75d0ecb8f558000ede985c1e5d2353790fec
tidianthony <167877348+tidianthony@users.noreply.github.com>
2024-09-09 15:10:17 +0200

    Doing basic calculations

---
+++ Basic_code_level1.py
@@ -0,0 +1,67 @@
+# Greetings.
+greetings = "Hello!, hope you are having a good day."
+print(greetings)
+
+# Playing with variables, input and casting.
+print("Playing with variables, input and casting: ")
+name = input("What is your name? ")
+age = input("How old are you? ")
+house_number = input("Enter your house number: ")
+street_name = input("Enter your street name: ")
+print(f"This is {name} and she is {age} years old. she lives at house number {house_number} on {street_name}")
+
+# Declearing variables.
+num_1 = 99, 23
+num_2 = 23
+num_3 = 150
+string_1 = "100"
+
+''' Cating the variables as follows:
+num_1 into an string
+num_2 into a float
+num_3 into a string
+string_1 into an integer '''
+
+num_1 = str(num_1)
+num_2 = float(num_2)
+num_3 = str(num_3)
+string_1 = int(string_1)
+
+# Printing the variables according to their cast type.
+print(" Declearing variables and Printing the variables according to their caTst type :")
+print(type(num_1))
+print(type(num_2))
+print(type(num_3))
+print(type(string_1))
+
+# Basic calculations.
+print("Basic calculations:")
+x = 5
+y = 4
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+a = 13
+b = 7
+c = 5
+result = (a + c * b + c - a)
+print(f"Answer: {result}")
+
+# Finding the sum and product of 12 and 5.
+print("Finding the sum and product: ")
+a = 12
+b = 5
+sum = (a + b)
+product = (a * b)
+remainder = (a / b)
+print(f"sum is: {sum}")
+print(f"product is: {product}")
+print(f"remendir is {remainder}")
+
+# Building a basic calculator.
+print("Calculator.")
+num1 = input("Enter a number")
+num2 = input("Enter a number")
+answer = (num1 + num2)
+print(f"Your answer is: {answer}")
